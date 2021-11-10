6ad1ddefa166ad57acfc67b66eab113698d907eb
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2021-11-10 22:52:28 +0100

    Add shooting and turret enemy type
    Add basic player shooting

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,18 +2,51 @@ using System;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
+public enum EnemyType {
+    Crawling,
+    Shooting,
+    Turret
+}
+
 public class Enemy : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] private GameObject _playerTarget;
     [SerializeField] private float _speed = 1f;
     [SerializeField] private float _rotateSpeed = 1f;
     [SerializeField] private Vector3 _positionNoise;
     [SerializeField] private float _maxNoise = 2f;
     
+    [Header("Attack")]
+    [SerializeField] private EnemyType _type;
+    [SerializeField] private int _damage = 50;
+    [SerializeField] private bool _destroyOnCollision = true;
+
+    [Header("If shooting or turret type")]
+    [SerializeField] private float _shootInterval = 1f;
+    [SerializeField] private float _shootNoiseInterval = 0.25f;
+    [SerializeField] private float _maxDistanceFromPlayer = 1000f;
+    [SerializeField] private GameObject _bullet;
+    
+    [Header("HP")] 
+    [SerializeField] private int _maxHP = 100;
+    [SerializeField] private int _currentHP;
+    
     private void Start()
     {
-        InvokeRepeating("Noise", 1f, 1f / 2);
+        if (_type == EnemyType.Crawling)
+        {
+            InvokeRepeating("Noise", 1f, 1f / 2);
+        }
+
+        if (_type == EnemyType.Shooting || _type == EnemyType.Turret)
+        {
+            InvokeRepeating("Shoot", 1f, _shootInterval + Random.Range(-_shootNoiseInterval, _shootNoiseInterval));
+        }
+
         RandomOnStart();
+
+        _currentHP = _maxHP;
     }
 
     private void RandomOnStart()
@@ -25,22 +58,28 @@ public class Enemy : MonoBehaviour
 
     private void SelectPlayerTarget()
     {
-        if (_playerTarget == null)
-        {
-            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-
-            int x = Random.Range(0, players.Length);
-            _playerTarget = players[x];
-        }
+        if (_playerTarget != null) return;
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        _playerTarget = players[Random.Range(0, players.Length)];
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
+        if (_currentHP < 0)
+        {
+            EnemyDie();
+            return;
+        }
+        
         SelectPlayerTarget();
 
         Vector3 targetPosition = _playerTarget.transform.position + _positionNoise;
-        
-        transform.localPosition = Vector3.MoveTowards (transform.localPosition, targetPosition, _speed * Time.deltaTime);
+
+        if (_type == EnemyType.Crawling || _type == EnemyType.Shooting)
+        {
+            transform.localPosition = Vector3.MoveTowards (transform.localPosition, targetPosition, _speed * Time.deltaTime);
+        }
+
         Vector3 targetDirection = targetPosition - transform.position;
         Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, _rotateSpeed * Time.deltaTime, 0.0f);
         transform.rotation = Quaternion.LookRotation(newDirection);
@@ -49,5 +88,40 @@ public class Enemy : MonoBehaviour
     private void Noise()
     {
         _positionNoise = new Vector3(Random.Range(-_maxNoise, _maxNoise), 0, Random.Range(-_maxNoise, _maxNoise));
+    }
+
+    public int GetDamage()
+    {
+        return _damage;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PlayerBullet"))
+        {
+            Bullet bullet = other.gameObject.GetComponent<Bullet>();
+
+            _currentHP -= bullet.GetDamage();
+            bullet.DestroyBullet();
+        }
+    }
+
+    public void CollideWithPlayer()
+    {
+        if (_destroyOnCollision)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject bullet = Instantiate(_bullet, transform.position, transform.rotation);
+        bullet.GetComponent<Bullet>().SetDamage(_damage);
+    }
+
+    private void EnemyDie()
+    {
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,15 +8,21 @@ public class Player : MonoBehaviour
     [SerializeField] private float _speed = 1f;
     [SerializeField] private float _speedMultiplier = 1f;
 
-
     [SerializeField] private KeyCode _up;
     [SerializeField] private KeyCode _down;
     [SerializeField] private KeyCode _left;
     [SerializeField] private KeyCode _right;
+    [SerializeField] private KeyCode _shoot;
+
+    [SerializeField] private int _maxHP = 1000;
+    [SerializeField] private int _currentHP;
+
+    [SerializeField] private GameObject _bullet;
+    [SerializeField] private int _damage = 100;
     
     void Start()
     {
-        
+        _currentHP = _maxHP;
     }
 
     void Update()
@@ -33,7 +40,35 @@ public class Player : MonoBehaviour
         {
             transform.rotation = Quaternion.LookRotation (inputs);
   
-            transform.Translate(inputs * _speed * _speedMultiplier, Space.World);
+            transform.Translate(inputs * _speed * _speedMultiplier * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKeyDown(_shoot))
+        {
+            GameObject bullet = Instantiate(_bullet, transform.position, transform.rotation);
+            bullet.GetComponent<Bullet>().SetDamage(_damage);
+        }
+    }
+
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Enemy enemy = other.gameObject.GetComponent<Enemy>();
+
+            _currentHP -= enemy.GetDamage();
+            enemy.CollideWithPlayer();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("EnemyBullet"))
+        {
+            Bullet bullet = other.gameObject.GetComponent<Bullet>();
+
+            _currentHP -= bullet.GetDamage();
+            bullet.DestroyBullet();
         }
     }
 }
