815fb4b5e9040c72b53d4bdfa4f985b259677155
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2021-11-10 22:52:28 +0100

    Add shooting and turret enemy type
    Add basic player shooting

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+   [SerializeField] private float _speed;
+   [SerializeField] private int _damage;
+   [SerializeField] private float _destroyAfter = 1f;
+
+   public void Start()
+   {
+      Invoke("DestroyBullet", _destroyAfter);
+   }
+
+   public void SetDamage(int damage)
+   {
+      _damage = damage;
+   }
+   
+   public int GetDamage()
+   {
+      return _damage;
+   }
+
+   public void FixedUpdate()
+   {
+      transform.Translate(transform.forward * _speed * Time.deltaTime, Space.World);
+   }
+   
+   public void DestroyBullet()
+   {
+      Destroy(gameObject);
+   }
+}
